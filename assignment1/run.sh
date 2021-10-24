c6186b52387010f8ed80c7830a5f7698bd5dd3ad
dee-vy <48962981+dee-vy@users.noreply.github.com>
2021-10-24 23:36:26 +0100

    added title for each result

--- assignment1/run.sh
+++ assignment1/run.sh
@@ -12,21 +12,21 @@ mvn clean install
 mvn compile exec:java -Dexec.mainClass="IndexFile"
 cd trec_eval
 
-
+echo "BM25_results_StandardEnglish"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../BM25_results_StandardEnglish.txt
-
+echo "BM25_results_Classic"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../BM25_results_Classic.txt
-
+echo "BM25_results_Whitespace"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../BM25_results_Whitespace.txt
-
+echo "BM25_results_Simple"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../BM25_results_Simple.txt
 
-
+echo "VSM_results_StandardEnglish"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../VSM_results_StandardEnglish.txt
-
+echo "VSM_results_Classic"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../VSM_results_Classic.txt
-
+echo "VSM_results_Whitespace"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../VSM_results_Whitespace.txt
-
+echo "VSM_results_Simple"
 ./trec_eval -m map -m gm_map -m P.5 -m num_ret -m num_rel -m num_rel_ret ../cranfield/QRelsCorrectedforTRECeval ../VSM_results_Simple.txt
 
